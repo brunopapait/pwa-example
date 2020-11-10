cca3d039509f2be2573fe60fa05d1aa8c30ee97c
Bruno Papait <brunohpapait@gmail.com>
2020-11-10 09:53:36 -0300

    Removendo consoles.logs ..

--- src/pages/NavBar/index.js
+++ src/pages/NavBar/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function NavBar({}) {
-  console.log(window.location.pathname);
   return (
     <div className='navbar'>
       <h3>Gerenciador de tarefas</h3>
